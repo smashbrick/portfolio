024b6653f3932d8925555f8222430ba78f4cea6e
Smashbrick <smashbrick@yahoo.com>
2024-09-05 21:43:43 +0530

    height and width changes to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ function Card(props) {
 	// eslint-disable-next-line react/prop-types
 	const { children } = props;
 	return (
-		<div className="h-[500px] w-[350px]  rounded-3xl grid  place-items-center  animated-card overflow-hidden relative shadow-dark mb-20 m-auto lg:m-0 ">
+		<div className="sm:h-[500px] sm:w-[350px] h-[450px] w-[300px]  rounded-3xl grid  place-items-center  animated-card overflow-hidden relative shadow-dark mb-20 m-auto lg:m-0 ">
 			<div className="grid z-10 w-full h-full p-10 grid-rows-[1fr_6fr]">
 				{children}
 			</div>
