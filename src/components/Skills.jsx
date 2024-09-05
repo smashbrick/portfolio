227c4d91f4a0e121e28f3984b8bf45c97f878252
Smashbrick <smashbrick@yahoo.com>
2024-09-06 01:06:20 +0530

    Refactored skills file 😮‍💨

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import Card from "./Card";
 import Section from "./Section";
 
@@ -13,72 +14,49 @@ const skills = {
 	Backend: ["Golang", "MongoDB", "Postgres", "Prisma"],
 	Tools: ["Git & Github", "Postman", "Figma", "VsCode"],
 };
+
+const SkillList = ({ category, items, iconSrc }) => (
+	<Card>
+		<h3 className="text-2xl font-semibold text-center">{category}</h3>
+		<div className="relative">
+			<ol className="text-xl w-full h-full flex flex-col gap-4">
+				{items.map((skill, index) => (
+					<li key={index} className="flex gap-4 items-center">
+						<img
+							src="/arrow-right-solid.svg"
+							alt="Arrow"
+							className="h-[1rem]"
+						/>
+						{skill}
+					</li>
+				))}
+			</ol>
+			{iconSrc && (
+				<img
+					src={iconSrc}
+					alt={`${category.toLowerCase()} icon`}
+					className="absolute w-[3rem] bottom-[-5%]"
+				/>
+			)}
+		</div>
+	</Card>
+);
+
 function Skills() {
 	return (
 		<Section sectionText="Explore my" sectionTitle="Skills" id="skills">
-			<div className=" lg:h-[88vh] h-auto sm:p-10 p-5 block lg:flex justify-around items-center">
-				{/* Card goes here */}
-				<Card>
-					<h3 className="text-2xl font-semibold text-center">Front-end</h3>
-					<div className="relative">
-						<ol className="text-xl w-full h-full flex flex-col gap-4">
-							{skills.Frontend.map((skill, index) => (
-								<li key={index} className="flex gap-4 items-center">
-									<img
-										src="/arrow-right-solid.svg"
-										alt="Checkmark"
-										className="h-[1rem]"
-									/>
-									{skill}
-								</li>
-							))}
-						</ol>
-						<img
-							src="/duck.png"
-							alt="Duck icon"
-							className="absolute w-[3rem] bottom-[-5%]"
-						/>
-					</div>
-				</Card>
-				<Card>
-					<h3 className="text-2xl  font-semibold text-center">Back-end</h3>
-					<div className="relative">
-						<ol className="text-xl w-full h-full flex flex-col gap-4 ">
-							{skills.Backend.map((skill, index) => (
-								<li key={index} className="flex gap-4 items-center">
-									<img
-										src="/arrow-right-solid.svg"
-										alt="Checkmark"
-										className="h-[1rem]"
-									/>
-									{skill}
-								</li>
-							))}
-						</ol>
-						<img
-							src="/cat.png"
-							alt="cat icon"
-							className="absolute w-[4rem] bottom-[-5%]"
-						/>
-					</div>
-				</Card>
-				<Card>
-					<h3 className="text-2xl  font-semibold text-center">Tools</h3>
-					<div>
-						<ol className="text-xl w-full h-full flex flex-col gap-4">
-							{skills.Tools.map((skill, index) => (
-								<li key={index} className="flex gap-4 items-center">
-									<img
-										src="/arrow-right-solid.svg"
-										alt="Checkmark"
-										className="h-[1rem]"
-									/>
-									{skill}
-								</li>
-							))}
-						</ol>
-					</div>
-				</Card>
+			<div className="lg:h-[88vh] h-auto sm:p-10 p-5 block lg:flex justify-around items-center">
+				<SkillList
+					category="Front-end"
+					items={skills.Frontend}
+					iconSrc="/duck.png"
+				/>
+				<SkillList
+					category="Back-end"
+					items={skills.Backend}
+					iconSrc="/cat.png"
+				/>
+				<SkillList category="Tools" items={skills.Tools} />
 			</div>
 		</Section>
 	);
