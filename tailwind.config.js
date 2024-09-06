5e3879448723188fc0663e5e3314589a7a723d66
Smashbrick <smashbrick@yahoo.com>
2024-09-06 19:03:55 +0530

    Added border between the sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
 			boxShadow: {
 				dark: "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;",
 				light: " rgba(149, 157, 165, 0.2) 0px 8px 24px",
+				top: "rgba(0, 0, 0, 0.1) 0px -2px 2px",
 			},
 		},
 	},
